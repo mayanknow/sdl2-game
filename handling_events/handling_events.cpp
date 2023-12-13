87fc038ccd8ed594fd9efee2a260572efd81c07d
Mayank Sharma <mayankmssharma18@gmail.com>
2023-12-13 18:59:37 +0000

    event handling updated

--- handling_events/handling_events.cpp
+++ handling_events/handling_events.cpp
@@ -37,8 +37,34 @@ int main(int argc, char* argv[])
         while(SDL_PollEvent(&event))
         {
             // handle each specific event
+            // SQL_QUIT is close button
             if(event.type == SDL_QUIT) {
                 gameIsRunning = false;
+            }
+            // SDL_ALLCAPITAL is a variable
+            // SDL_IfItLooksLikeThis is a function
+            if(event.type == SDL_MOUSEMOTION) {
+                std::cout << "mouse has been moved\n";
+            }
+
+            // SDL_KEYDOWN checks for any event emitted related to keys
+            if(event.type == SDL_KEYDOWN) {
+                // event.key.keysm.sym
+                // keysym is a key symbol union which has a sym value
+                // SDLK_0 is teh 0 key on the keyboard
+                if(event.key.keysym.sym == SDLK_0) {
+                    std::cout << "0 was pressed\n";
+                } else {
+                    std::cout << "0 was not pressed\n";
+                }
+            }
+
+            // getting input values from the keyborad snapshort
+            // Uint8 is an unsigned integer
+            // No negative values are expected so unsigned
+            const Uint8 *state = SDL_GetKeyboardState(NULL);
+            if(state[SDL_SCANCODE_RIGHT]) {
+                std::cout << "Right arrow key is pressed\n";
             }
         }
     }
