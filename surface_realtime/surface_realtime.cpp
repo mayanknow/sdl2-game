b78604df08bb0bebfc3c95c59e0b2029fe1cef55
Mayank Sharma <mayankmssharma18@gmail.com>
2023-12-15 19:24:37 +0000

    sdl_renderer_draw added

--- surface_realtime/surface_realtime.cpp
+++ surface_realtime/surface_realtime.cpp
@@ -6,6 +6,36 @@
 // importing SDL2 header
 #include<SDL.h>
 
+// set-pixel function
+// uint8_t is unsigned 8-bit integer non-negative values
+void SetPixel(SDL_Surface *surface, int x, int y, uint8_t r, uint8_t g, uint8_t b)
+{
+    SDL_LockSurface(surface);
+    std::cout << "Left mouse pressed(" << x << ", " << y << ")\n";
+    uint8_t *pixelArray = (uint8_t *) surface->pixels;
+    // here pitch is the total number of bytes in  a row
+    /*      x
+        ------------->
+        |  1 2 3 4 5
+      y |  1 2 X 4 5
+        |  1 2 3 4 5
+        \/
+        There are 5 elements in each row
+        so the pitch will be 6
+        **
+        to reach the 3rd element of 2nd row
+        we multiply y with pitch and add the element
+        X is the third element
+        **
+    */
+
+    // BGR format is used here
+    pixelArray[y*surface->pitch +x*surface->format->BytesPerPixel+0] = b;
+    pixelArray[y*surface->pitch +x*surface->format->BytesPerPixel+1] = g;
+    pixelArray[y*surface->pitch +x*surface->format->BytesPerPixel+2] = r;
+    SDL_UnlockSurface(surface);
+}
+
 int main(int argc, char* argv[])
 {
     // creating a window data type
@@ -63,22 +93,30 @@ int main(int argc, char* argv[])
             if(event.type == SDL_QUIT) {
                 gameIsRunning = false;
             }
-            if(event.button.button == SDL_BUTTON_LEFT) {
-                SDL_LockSurface(screen);
-                std::cout << "Left mouse pressed(" << x << ", " << y << ")\n";
-                SDL_memset(screen->pixels, 255, screen->h * screen->pitch);
-                SDL_UnlockSurface(screen);
-                SDL_UpdateWindowSurface(window);
-            }
+            // if(event.button.button == SDL_BUTTON_LEFT) {
+            //     SetPixel(screen, x, y, 255, 0 ,0);
+            // }
 
             // getting input values from the keyborad snapshort
             // Uint8 is an unsigned integer
             // No negative values are expected so unsigned
             const Uint8 *state = SDL_GetKeyboardState(NULL);
-            if(state[SDL_SCANCODE_RIGHT]) {
+            /*
+                **MORTAL KOMBAT COMBO** 
+            */
+            // Right button + left click => red color
+            if(state[SDL_SCANCODE_RIGHT] && event.button.button == SDL_BUTTON_LEFT) {
+                SetPixel(screen, x, y, 255, 0 ,0);
+                std::cout << "Left arrow key is pressed\n";
+            }
+            // Left button + left click => green color
+            if(state[SDL_SCANCODE_LEFT] && event.button.button == SDL_BUTTON_LEFT) {
+                SetPixel(screen, x, y, 0, 255 ,0);
                 std::cout << "Right arrow key is pressed\n";
             }
         }
+        // updating window ever frame in game loop
+        SDL_UpdateWindowSurface(window);
     }
 
     // destroying created window
