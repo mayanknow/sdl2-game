9440953343101fd952f8ecf53820741be7212de3
Mayank Sharma <mayankmssharma18@gmail.com>
2023-12-23 13:27:45 +0000

    minor changes

--- texture_rectangles_sdl/build.sh
+++ texture_rectangles_sdl/build.sh
@@ -1,2 +1,2 @@
-g++  -std=c++17 ./src/*.cpp -I./include -o ./bin/app -I/Library/Frameworks/SDL2.framework/Headers -F/Library/Frameworks -framework SDL2
+g++  -std=c++17 ./src/*.cpp -I./include -o ./bin/prog -I/Library/Frameworks/SDL2.framework/Headers -F/Library/Frameworks -framework SDL2
 install_name_tool -add_rpath /Library/Frameworks/ ./bin/prog
